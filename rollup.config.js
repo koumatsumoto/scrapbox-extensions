190dcf22b59193d87fa99848df55ee21ec5abb25
kou.matsumoto <kou.matsumoto.jp@gmail.com>
2019-12-07 01:53:45 +0900

    feat: migrate current custom scripts

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript';
+import { terser } from "rollup-plugin-terser";
+import {main} from "./src/main";
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    name: 'bundle',
+    file: 'dist/bundle.min.js',
+    // cannot use `iife` bundled code as scrapbox custom script
+    format: 'esm',
+  },
+  plugins: [
+    typescript(),
+    terser(),
+  ],
+};
