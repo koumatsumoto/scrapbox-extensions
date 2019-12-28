c8c71803146cf27f85d3fd633cd1c06fc7e05cbe
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2019-12-29 05:44:33 +0900

    format data

--- src/scripts/use-debug-board/use-debug-board.ts
+++ src/scripts/use-debug-board/use-debug-board.ts
@@ -2,34 +2,41 @@ import { getDeviceMotionStream } from '../../libs/common';
 import { componentManager } from '../component-manager';
 import { MyDebugBoard } from '../../components';
 
+const fixOrNull = (val: number | null) => {
+  if (val === null) {
+    return null;
+  }
+
+  return val.toFixed(3);
+};
+
+const formatDeviceMotionEvent = (e: DeviceMotionEvent) => {
+  const acceleration = e.acceleration || ({} as any);
+  const rotationRate = e.rotationRate || ({} as any);
+  const interval = e.interval;
+
+  return {
+    interval,
+    acceleration: {
+      x: fixOrNull(acceleration.x),
+      y: fixOrNull(acceleration.y),
+      z: fixOrNull(acceleration.z),
+    },
+    rotationRate: {
+      alpha: fixOrNull(rotationRate.alpha),
+      beta: fixOrNull(rotationRate.beta),
+      gamma: fixOrNull(rotationRate.gamma),
+    },
+  } as const;
+};
+
 export const useDebugBoard = () => {
   const motion$ = getDeviceMotionStream();
   const debugBoard = componentManager.getInstance(MyDebugBoard);
 
   motion$.subscribe((e: DeviceMotionEvent) => {
-    console.log(e);
-    const acceleration = e.acceleration || ({} as any);
-    const rotationRate = e.rotationRate || ({} as any);
-    const interval = e.interval;
+    const data = formatDeviceMotionEvent(e);
 
-    debugBoard.updateText(
-      JSON.stringify(
-        {
-          interval,
-          acceleration: {
-            x: acceleration.x,
-            y: acceleration.y,
-            z: acceleration.z,
-          },
-          rotationRate: {
-            alpha: rotationRate.alpha,
-            beta: rotationRate.beta,
-            gamma: rotationRate.gamma,
-          },
-        },
-        null,
-        2,
-      ),
-    );
+    debugBoard.updateText(JSON.stringify(data, null, 2));
   });
 };
