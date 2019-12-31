3fac84590c4603bf2dcc0d54ad76cf8d5e913b10
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2019-12-31 23:03:27 +0900

    dev toManipulation

--- src/libs/common/deviceorientation-and-devicemotion/make-command.ts
+++ src/libs/common/deviceorientation-and-devicemotion/make-command.ts
@@ -2,11 +2,13 @@ import { Observable } from 'rxjs';
 import { OrientationAndMotionSummary } from './types';
 import { getRx } from '../rxjs';
 
-export const makeCommand = () => (source: Observable<OrientationAndMotionSummary>) => {
+const makeManipulation = (values: [OrientationAndMotionSummary, OrientationAndMotionSummary]) => {
+  const prev = values[0];
+  const curr = values[1];
+};
+
+export const toManipulation = () => (source: Observable<OrientationAndMotionSummary>) => {
   const { map, pairwise } = getRx().operators;
 
-  return source.pipe(
-    pairwise(),
-    map(([prev, curr]) => {}),
-  );
+  return source.pipe(pairwise(), map(makeManipulation));
 };
