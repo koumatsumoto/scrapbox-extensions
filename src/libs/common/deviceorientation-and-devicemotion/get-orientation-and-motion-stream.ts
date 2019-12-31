ede6451df04c2c72c9207869bb6ca441e93ad310
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-01 01:05:50 +0900

    feat(libs): improve horizontal manipulation

--- src/libs/common/deviceorientation-and-devicemotion/get-orientation-and-motion-stream.ts
+++ src/libs/common/deviceorientation-and-devicemotion/get-orientation-and-motion-stream.ts
@@ -1,6 +1,6 @@
 import { getDeviceMotionStream } from './devicemotion';
 import { getDeviceOrientationStream } from './deviceorientation/get-device-orientation-stream';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { DeviceMotion, DeviceOrientation, OrientationAndMotionSummary, Precision } from './types';
 import { getRx } from '../rxjs';
 import { summarizeMotions } from './devicemotion/internal/summarize';
@@ -8,9 +8,8 @@ import { fixValue } from './fix-value';
 import { forDebug } from './for-debug';
 import { toManipulation } from './make-manipulation';
 
-const defaultOption = {
-  interval: 200,
-};
+// use if needed
+const defaultOption = {};
 
 /**
  * @param options
@@ -18,9 +17,7 @@ const defaultOption = {
  * @param motion$ - for testing
  */
 export const getOrientationAndMotionStream = (
-  option: {
-    interval: number;
-  } = defaultOption,
+  option: {} = defaultOption,
   orientation$: Observable<DeviceOrientation> = getDeviceOrientationStream(),
   motion$: Observable<DeviceMotion> = getDeviceMotionStream(),
 ) => {
@@ -29,33 +26,57 @@ export const getOrientationAndMotionStream = (
   return new Observable<OrientationAndMotionSummary>((subscriber) => {
     let orientations: DeviceOrientation[] = [];
     let motions: DeviceMotion[] = [];
+    let orientationSubscription: Subscription | undefined;
+    let motionSubscription: Subscription | undefined;
+    let setIntervalId: number | undefined;
+
+    const registerInterval = (motionInterval: number) => {
+      // time to process accumulated event data
+      // process 5 motion events at once (temporary implementation)
+      const processInterval = motionInterval * 5 + 1;
+
+      setIntervalId = window.setInterval(() => {
+        if (orientations.length < 1 || motions.length < 1) {
+          return;
+        }
+
+        const orientation = orientations[orientations.length - 1]!;
+        const interval = motions[0].interval; // interval never change in same device
+        const summary = summarizeMotions(motions)!;
+        orientations = [];
+        motions = [];
+
+        subscriber.next({
+          orientation,
+          motion: {
+            interval,
+            acceleration: summary.acceleration,
+            accelerationIncludingGravity: summary.accelerationIncludingGravity,
+            rotationRate: summary.rotationRate,
+          },
+        });
+      }, processInterval);
+    };
 
     orientation$.subscribe((v) => orientations.push(v));
-    motion$.subscribe((v) => motions.push(v));
-
-    const id = setInterval(() => {
-      if (orientations.length < 1 || motions.length < 1) {
-        return;
+    motion$.subscribe((v) => {
+      if (setIntervalId === undefined) {
+        registerInterval(v.interval);
       }
+      motions.push(v);
+    });
 
-      const orientation = orientations[orientations.length - 1]!;
-      const interval = motions[0].interval; // interval never change in same device
-      const summary = summarizeMotions(motions)!;
-      orientations = [];
-      motions = [];
-
-      subscriber.next({
-        orientation,
-        motion: {
-          interval,
-          acceleration: summary.acceleration,
-          accelerationIncludingGravity: summary.accelerationIncludingGravity,
-          rotationRate: summary.rotationRate,
-        },
-      });
-    }, option.interval);
-
-    return () => clearInterval(id);
+    return () => {
+      if (setIntervalId !== undefined) {
+        clearInterval(setIntervalId);
+      }
+      if (orientationSubscription) {
+        orientationSubscription.unsubscribe();
+      }
+      if (motionSubscription) {
+        motionSubscription.unsubscribe();
+      }
+    };
   });
 };
 
