cf5826c74aff0b7a8916d8c8d936ea48f7a444a1
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2019-12-31 20:56:26 +0900

    update

--- src/libs/common/deviceorientation-and-devicemotion/deviceorientation/internal/calculate-average.spec.ts
+++
@@ -1,8 +0,0 @@
-import { calculateAverageAsInt } from './calculate-change';
-import { createTestingDeviceMotionValue as v } from '../../test-helpers';
-
-describe('calculateAverageAsInt', () => {
-  it('should calculate well', () => {
-    expect(calculateAverageAsInt([v(1), v(2), v(3)])).toEqual(v(2));
-  });
-});
