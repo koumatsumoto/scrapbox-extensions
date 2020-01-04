25eeebc1dfcb1bfe2d3daf6d75b6e2f4b5b019c6
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-04 19:58:25 +0900

    update

--- src/libs/common/deviceorientation-and-devicemotion/new-impl/get-motion-set-stream.ts
+++ src/libs/common/deviceorientation-and-devicemotion/new-impl/get-motion-set-stream.ts
@@ -3,49 +3,33 @@ import { DeviceMotion, DeviceOrientation } from '../types';
 import { getDeviceOrientationStream } from '../deviceorientation/get-device-orientation-stream';
 import { getDeviceMotionStream } from '../devicemotion';
 import { getRx, withHistory } from '../../rxjs';
-import { roundToInt } from '../../arithmetic';
 import { combine } from './movement/combine';
 import { ActionTypes } from './types';
 import { isTap } from './action/tap';
 import { classify, Movement } from './movement/classify-movement';
 import { simplifyMovements } from './action/util';
 
-export const get4MotionWithOrientationStream = (
+export const getMovementStream = (
   orientation$: Observable<DeviceOrientation> = getDeviceOrientationStream(),
   motion$: Observable<DeviceMotion> = getDeviceMotionStream(),
-) => {
-  const { bufferCount, withLatestFrom } = getRx().operators;
+): Observable<{ sid: number; data: Movement }> => {
+  const { bufferCount, map, withLatestFrom } = getRx().operators;
 
-  return motion$.pipe(bufferCount(4), withLatestFrom(orientation$));
-};
-
-// to check data by same sid
-let singletonToDebug: Observable<{ sid: number; direction: string; data: Movement }>;
-export const getMovementStream = () => {
-  const { map } = getRx().operators;
   let sid = 0;
-
-  if (singletonToDebug) {
-    return singletonToDebug;
-  }
-
-  singletonToDebug = get4MotionWithOrientationStream().pipe(
+  return motion$.pipe(
+    bufferCount(4),
+    withLatestFrom(orientation$),
     map(([motions, orientation]) => {
       const gammas = motions.map((m) => m.rotationRate.gamma);
       const aggregation = combine(orientation.gamma, gammas);
       const data = classify(aggregation);
 
-      const direction = orientation.gamma > 0 ? 'right' : 'left';
-
       return {
         sid: sid++,
-        direction,
         data,
       };
     }),
   );
-
-  return singletonToDebug;
 };
 
 export const debug3 = () => {
@@ -62,65 +46,32 @@ export const debug3 = () => {
   );
 };
 
-/**
- * orientation and motion aggregation raw data for gamma
- */
-export const debug4 = () => {
-  const { map } = getRx().operators;
-
-  return get4MotionWithOrientationStream().pipe(
-    map(([motions, orientation]) => {
-      const gammas = motions.map((m) => m.rotationRate.gamma);
-      const aggregation = combine(orientation.gamma, gammas);
-
-      return {
-        gamma: roundToInt(orientation.gamma),
-        aggregation,
-      };
-    }),
-  );
-};
-
 type CommandData = {
   command: ActionTypes;
   // [first, last]
-  sid?: [number, number];
+  sid?: number[];
 };
 
 export const getMotionCommandStream = () => {
   const { Observable } = getRx();
   const { map } = getRx().operators;
-  const minimumRequiredCount = 3;
 
   return new Observable<CommandData>((subscriber) => {
-    let actionSubmittedId = -1;
     getMovementStream()
       .pipe(
-        withHistory(8),
+        withHistory(10),
         map((items) => {
-          // targets at least 1 item
-          const targets = items.filter((m) => m.sid > actionSubmittedId);
-          if (targets.length < minimumRequiredCount) {
-            return {
-              command: 'waiting',
-            };
-          }
+          const movements = items.map((m) => m.data);
 
-          const first = targets[0];
-          const last = targets[targets.length - 1];
-          const tap = isTap(targets.map((m) => m.data));
           let actionType: ActionTypes = 'none';
+          const tap = isTap(movements);
           if (tap) {
             actionType = 'tap';
-            actionSubmittedId = last.sid;
-          } else {
-            actionSubmittedId = first.sid;
           }
 
           return {
             command: actionType,
-            sid: [first.sid, last.sid],
-            debug: simplifyMovements(targets.map((m) => m.data)),
+            sid: [items[0].sid, items[items.length - 1].sid],
           };
         }),
       )
