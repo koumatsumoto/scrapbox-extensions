bd536c2bc7a2ac54e22591eb84296a5fe7952ced
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-04 19:28:06 +0900

    adjust

--- src/libs/common/deviceorientation-and-devicemotion/new-impl/action/tap.spec.ts
+++ src/libs/common/deviceorientation-and-devicemotion/new-impl/action/tap.spec.ts
@@ -7,7 +7,6 @@ describe('isTap', () => {
   const downBased = (xyz: [number, number, number]) => createMovements(xyz, 'down') as [Movement, Movement, Movement];
 
   it.each([[upBased], [downBased]])('detect tap', (v: typeof upBased | typeof downBased) => {
-    expect(isTap(v([0, 3, 1]))).toBe(true);
     expect(isTap(v([0, 3, 0]))).toBe(true);
     expect(isTap(v([0, 3, -1]))).toBe(true);
     expect(isTap(v([0, 3, -2]))).toBe(true);
@@ -15,9 +14,6 @@ describe('isTap', () => {
     expect(isTap(v([1, 3, -1]))).toBe(true);
     expect(isTap(v([1, 3, -2]))).toBe(true);
     expect(isTap(v([1, 3, -3]))).toBe(true);
-    expect(isTap(v([1, -2, 1]))).toBe(true);
-    expect(isTap(v([1, -2, 2]))).toBe(true);
-    expect(isTap(v([1, -2, 3]))).toBe(true);
     expect(isTap(v([1, -3, 0]))).toBe(true);
     expect(isTap(v([1, -3, 1]))).toBe(true);
     expect(isTap(v([1, -3, 2]))).toBe(true);
