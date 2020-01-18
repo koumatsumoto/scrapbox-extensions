28b97c367c52531cbd3a737ccc7acfcedf37f929
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-18 13:57:28 +0900

    update

--- src/libs/scrapbox/public-api/scrapbox.ts
+++ src/libs/scrapbox/public-api/scrapbox.ts
@@ -2,15 +2,9 @@ import '../../../types/scrapbox';
 
 export const getScrapbox = () => window.scrapbox;
 
-export const getCurrentProjectName = () => {
-  getScrapbox().Project.name;
-};
+export const getCurrentProjectName = () => getScrapbox().Project.name;
 
-export const getCurrentPageName = () => {
-  getScrapbox().Page.title;
-};
+export const getCurrentPageName = () => getScrapbox().Page.title;
 
-// after api response on react bootstrap
-export const isScrapboxReady = () => {
-  return getCurrentProjectName() !== undefined && getCurrentPageName() !== undefined;
-};
+// ready after api requests initiated on react bootstrap completed
+export const isScrapboxReady = () => getCurrentProjectName() !== undefined && getCurrentPageName() !== undefined;
