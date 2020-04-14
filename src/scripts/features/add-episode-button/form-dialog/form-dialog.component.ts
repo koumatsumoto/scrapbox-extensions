9e30fa13ca40a17c9d4adabfa63504c0e6528bf9
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-04-15 02:23:29 +0900

    update

--- src/scripts/features/add-episode-button/form-dialog/form-dialog.component.ts
+++ src/scripts/features/add-episode-button/form-dialog/form-dialog.component.ts
@@ -24,7 +24,7 @@ const makeCheckboxesHTML = (tagOptions: TagOption[]) => {
     .join('');
 };
 
-const hideCSSClass = '-hide'; // display: none
+const indicatorShowingClass = '-show-indicator'; // used in css
 
 export class MyTagFormDialog extends HTMLElement {
   // whether form submitted or canceled
@@ -33,19 +33,15 @@ export class MyTagFormDialog extends HTMLElement {
   // used in css also
   static readonly elementName = 'add-episode-form-dialog';
   private dialog: HTMLDialogElement;
-  private form: HTMLFormElement;
   private checkboxContainer: HTMLDivElement;
   private textInput: HTMLInputElement;
-  private loadingIndicatorContainer: HTMLDivElement;
 
   constructor(tagOptions: TagOption[]) {
     super();
 
     this.innerHTML = `${html}`;
     this.dialog = this.querySelector<HTMLDialogElement>('dialog')!;
-    this.form = this.querySelector<HTMLFormElement>('form')!;
     this.textInput = this.querySelector<HTMLInputElement>('input[type=text]')!;
-    this.loadingIndicatorContainer = this.querySelector<HTMLDivElement>('.loading-indicator-container')!;
 
     // construct DOM of checkboxes
     this.checkboxContainer = this.querySelector<HTMLDivElement>('#checkboxContainer')!;
@@ -97,7 +93,6 @@ export class MyTagFormDialog extends HTMLElement {
 
   // in api requesting
   showLoadingIndicator() {
-    this.form.classList.add(hideCSSClass);
-    this.loadingIndicatorContainer.classList.remove(hideCSSClass);
+    this.classList.add(indicatorShowingClass);
   }
 }
