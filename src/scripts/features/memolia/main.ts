2af719f8c731f6b3b2ee6c1f750c6124b76d6a36
kou.matsumoto <matsumoto@bitcoinbank.co.jp>
2020-04-22 10:14:08 +0900

    update

--- src/scripts/features/memolia/main.ts
+++ src/scripts/features/memolia/main.ts
@@ -8,7 +8,6 @@ export const useMemolia = () => {
       if (prevCount !== window.scrapbox.Page.lines.length) {
         prevCount = window.scrapbox.Page.lines.length;
         const blocks = makeBlocks(window.scrapbox.Page.lines);
-        console.log('[dev] memolia', window.scrapbox.Page.title, blocks);
         console.log('[dev] memolia blocks', blocks);
         console.log(
           '[dev] memolia episode parsed',
