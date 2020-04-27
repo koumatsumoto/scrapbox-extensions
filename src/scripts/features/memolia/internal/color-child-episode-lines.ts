ce79efeb13c0f5dc85d1a181704854e7b8ad7b82
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-04-27 21:54:21 +0900

    update

--- src/scripts/features/memolia/internal/color-child-episode-lines.ts
+++ src/scripts/features/memolia/internal/color-child-episode-lines.ts
@@ -1,12 +1,12 @@
 import { compact, map } from 'fp-ts/es6/Array';
 import { pipe } from 'fp-ts/es6/pipeable';
-import { addClass, findElementById } from '../../../../libs/common/dom';
+import { findElementById, updateDataset } from '../../../../libs/common/dom';
 import { Line, Memory } from '../types';
 
 // id to dom starts with 'L' char because raw line-id can start with number char (it's invalid for dom)
 const toDOMId = (lineId: string) => `L` + lineId;
 
-export const colorLines = (lines: Line[], className: string[]) =>
+export const colorLines = (lines: Line[], key: string, colorType: string) =>
   pipe(
     // Line object
     lines,
@@ -18,7 +18,7 @@ export const colorLines = (lines: Line[], className: string[]) =>
     compact,
     // side-effect
     // TODO(2020-04027): use function to execute side-effect rather than map
-    map((elem) => addClass(elem, className)),
+    map((elem) => updateDataset(elem, key, colorType)),
   );
 
 // for background color for each child episode block
@@ -26,7 +26,7 @@ const getNextCSSClassGen = () => {
   let idx = 0;
 
   return () => {
-    return `sx-bgc-${idx++ % 10}`;
+    return `type-${idx++ % 10}`;
   };
 };
 
@@ -35,9 +35,10 @@ export const colorChildEpisodeLines = (memory: Memory) => {
   memory.episodes
     .filter((ep) => ep.children.length)
     .forEach((ep) => {
-      // use random class
-      const colorCSSClass = 'sx-child-episode-line';
+      // data[sx-child-episode-line]
+      const colorDatasetKey = 'sxChildEpisodeLine';
+      const colorType = generator();
       console.log('[dev] children foreach', ep.children);
-      ep.children.forEach((child) => colorLines(child.lines, [colorCSSClass, generator()]));
+      ep.children.forEach((child) => colorLines(child.lines, colorDatasetKey, colorType));
     });
 };
