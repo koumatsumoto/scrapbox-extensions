78dc9e41f90da5b63e92bde8ea88d71c8db4f239
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-04-28 03:28:17 +0900

    feat(dynamic-config): introduce dynamic-config

---
+++ src/scripts/features/dynamic-config/main.ts
@@ -0,0 +1,100 @@
+import { chain, Either, fromPredicate, getOrElse, left, map, parseJSON, right, tryCatch } from 'fp-ts/es6/Either';
+import { pipe } from 'fp-ts/es6/pipeable';
+import { ApiClient } from '../../../libs/scrapbox/private-api/api-client/api-client';
+import { ApiResultPageLine, PageResponse } from '../../../libs/scrapbox/private-api/api-client/api-client-types';
+
+type ConfigObject = {
+  // 2020-04-28
+  // TODO: not implemented
+};
+
+type ConfigInLocalStorage = {
+  data: ConfigObject;
+  time: string; // Date.
+};
+
+const errors = {
+  pageNotFound: '[sx/dynamic-config] error to request config page, create /config page to use this feature.',
+  pageNotContainingCodeBlock: '[sx/dynamic-config] page not containing line that text including "code:".',
+  invalidPageContents: '[sx/dynamic-config] invalid json, check contents in config page, it can be errored if an empty line not existed.',
+};
+
+const storageKey = '[sx/dynamic-config] config';
+
+export const fetchConfigPage = async (): Promise<Either<Error, PageResponse>> => {
+  try {
+    const api = new ApiClient();
+    return right(await api.getPage('config'));
+  } catch (e) {
+    return left(new Error(errors.pageNotFound));
+  }
+};
+
+const isCodeBlockStartLine = (line: ApiResultPageLine) => line.text.includes('code:');
+const isCodeBlockEndLine = (line: ApiResultPageLine) => line.text === '';
+
+export const constructCodeStringsOrEmptyStrings = (lines: ApiResultPageLine[]) => {
+  // code:filename.json
+  let codeBlockHeadLineFound = true;
+  let totalCodeStrings = '';
+  for (const line of lines) {
+    switch (true) {
+      // from first-line to first code-block found
+      case !codeBlockHeadLineFound && isCodeBlockStartLine(line): {
+        codeBlockHeadLineFound = true;
+        break;
+      }
+      // from next of code-block found to before of code-block end
+      case codeBlockHeadLineFound && !isCodeBlockEndLine(line): {
+        totalCodeStrings += line.text + '\n';
+        break;
+      }
+      case codeBlockHeadLineFound && isCodeBlockEndLine(line): {
+        return totalCodeStrings;
+      }
+    }
+  }
+
+  return totalCodeStrings;
+};
+
+export const handleConfigObject = () =>
+  pipe(
+    map((response) => constructCodeStringsOrEmptyStrings(response.lines)),
+    fromPredicate(
+      (json) => json !== '',
+      (json) => new Error(errors.pageNotContainingCodeBlock + json),
+    ),
+    chain((json) => parseJSON<ConfigObject>(json, (e) => new Error(errors.invalidPageContents + e))),
+    chain((obj) => storeToStorage(obj)),
+  );
+
+export const storeToStorage = (data: ConfigObject, w = window) =>
+  tryCatch(
+    () => {
+      w.localStorage.setItem(
+        storageKey,
+        JSON.stringify({
+          data,
+          time: new Date().toISOString(),
+        }),
+      );
+    },
+    (e) => e,
+  );
+
+export const main = async () => {
+  const ma = await fetchConfigPage();
+
+  pipe(
+    ma,
+    getOrElse((e) => e),
+    map((response) => constructCodeStringsOrEmptyStrings(response.lines)),
+    fromPredicate(
+      (json) => json !== '',
+      (json) => new Error(errors.pageNotContainingCodeBlock + json),
+    ),
+    chain((json) => parseJSON<ConfigObject>(json, (e) => new Error(errors.invalidPageContents + e))),
+    chain((obj) => storeToStorage(obj)),
+  );
+};
