0f99c51c8ff3e957272090fa9a8e2b4106378d88
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-04-28 09:48:05 +0900

    update

--- src/scripts/features/dynamic-config/main.ts
+++ src/scripts/features/dynamic-config/main.ts
@@ -1,7 +1,10 @@
-import { chain, Either, fromPredicate, left, map, parseJSON, right, tryCatch } from 'fp-ts/es6/Either';
 import { pipe } from 'fp-ts/es6/pipeable';
 import { ApiClient } from '../../../libs/scrapbox/private-api/api-client/api-client';
 import { ApiResultPageLine, PageResponse } from '../../../libs/scrapbox/private-api/api-client/api-client-types';
+import { map, tryCatch } from 'fp-ts/es6/TaskEither';
+import { tryCatch as tc } from 'fp-ts/es6/Either';
+import { toError } from 'fp-ts/es6/Either';
+import { Lazy } from 'fp-ts/es6/function';
 
 type ConfigObject = {
   // 2020-04-28
@@ -21,21 +24,12 @@ const errors = {
 
 const storageKey = '[sx/dynamic-config] config';
 
-export const fetchConfigPage = async (): Promise<Either<Error, PageResponse>> => {
-  try {
-    const api = new ApiClient();
-    return right(await api.getPage('config'));
-  } catch (e) {
-    return left(new Error(errors.pageNotFound));
-  }
-};
-
 const isCodeBlockStartLine = (line: ApiResultPageLine) => line.text.includes('code:');
 const isCodeBlockEndLine = (line: ApiResultPageLine) => line.text === '';
 
 export const constructCodeStringsOrEmptyStrings = (lines: ApiResultPageLine[]) => {
   // code:filename.json
-  let codeBlockHeadLineFound = true;
+  let codeBlockHeadLineFound = false;
   let totalCodeStrings = '';
   for (const line of lines) {
     switch (true) {
@@ -58,29 +52,33 @@ export const constructCodeStringsOrEmptyStrings = (lines: ApiResultPageLine[]) =
   return totalCodeStrings;
 };
 
-export const storeToStorage = (data: ConfigObject, w = window) =>
-  tryCatch(
-    () => {
-      w.localStorage.setItem(
-        storageKey,
-        JSON.stringify({
-          data,
-          time: new Date().toISOString(),
-        }),
-      );
-    },
-    (e) => e,
+export const constructConfig = (json: string) => {
+  if (json === '') {
+    throw new Error(errors.invalidPageContents + json);
+  }
+
+  const config = JSON.parse(json) as ConfigInLocalStorage;
+  // TODO: validate config object here
+
+  return config;
+};
+
+export const storeToStorage = (data: ConfigObject, w = window) => {
+  w.localStorage.setItem(
+    storageKey,
+    JSON.stringify({
+      data,
+      time: new Date().toISOString(),
+    }),
   );
+};
 
 export const useDynamicConfig = async () =>
   pipe(
-    await fetchConfigPage(),
-    chain((v) => v),
-    map((response) => constructCodeStringsOrEmptyStrings(response.lines)),
-    fromPredicate(
-      (json) => json !== '',
-      (json) => new Error(errors.pageNotContainingCodeBlock + json),
-    ),
-    chain((json) => parseJSON<ConfigObject>(json, (e) => new Error(errors.invalidPageContents + e))),
-    chain((obj) => storeToStorage(obj)),
+    () => new ApiClient().getPage('config'),
+    (task: Lazy<Promise<PageResponse>>) => tryCatch(task, toError),
+    map((res: PageResponse) => res.lines),
+    map(constructCodeStringsOrEmptyStrings),
+    map(constructConfig),
+    (task) => tryCatch(task, toError),
   );
