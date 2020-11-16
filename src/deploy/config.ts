30b4f49f0e5573c3fbfa511aa3f4e8f15070badf
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-11-16 22:33:15 +0900

    feat: update deploy scripts

--- src/deploy/config.ts
+++ src/deploy/config.ts
@@ -5,12 +5,16 @@ const getDistDirPath = (file: string) => path.join(process.cwd(), 'dist', file);
 export const config = {
   deployTargets: [
     {
+      userId: '5dc685a50fc39d0017e27558',
+      projectId: '5dc685a50fc39d0017e27559',
       projectName: 'km-study',
       targetPageName: 'kou',
       codeBlockLabel: 'script.js',
       sourceFilePath: getDistDirPath('km-study.min.js'),
     },
     {
+      userId: '5dc685a50fc39d0017e27558',
+      projectId: '5dc685a50fc39d0017e27559',
       projectName: 'km-study',
       targetPageName: 'settings',
       codeBlockLabel: 'style.css',
