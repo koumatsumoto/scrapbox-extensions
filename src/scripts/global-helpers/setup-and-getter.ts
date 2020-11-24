3215de10d789afedb1589eb772d3c466412aed4f
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-11-24 21:14:05 +0900

    refactor: use Router from scrapbox-tools (#195)

--- src/scripts/global-helpers/setup-and-getter.ts
+++ src/scripts/global-helpers/setup-and-getter.ts
@@ -1,14 +1,25 @@
+import { Router } from 'scrapbox-tools/router';
 import { getScrapboxClient, ScrapboxClient } from 'scrapbox-tools/scrapbox-client';
-import { logOnly } from '../../libs/common/logger';
 import { getCurrentProjectName } from '../../libs/scrapbox/browser-api';
 
 let gettingClient: Promise<ScrapboxClient>;
+let router: Router;
 export const setupGlobalHelpers = () => {
-  getScrapboxClient({ projectName: getCurrentProjectName() }).catch(logOnly);
+  gettingClient = getScrapboxClient({ projectName: getCurrentProjectName() });
+  router = new Router({ debug: true });
 };
 
+const isSetupCompleted = () => !gettingClient || !router;
+
 export const getGlobalHelpers = async () => {
+  if (!isSetupCompleted()) {
+    setupGlobalHelpers();
+  }
+
   const [scrapboxClient] = await Promise.all([gettingClient]);
 
-  return { scrapboxClient };
+  return {
+    scrapboxClient,
+    router,
+  };
 };
