1948060135db52841e8094264694070172f7f8ec
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2021-06-03 20:37:40 +0900

    update

--- src/scripts/list-view-customization/show-datetime.spec.ts
+++ src/scripts/list-view-customization/show-datetime.spec.ts
@@ -1,8 +1,8 @@
 import { getPageTitleMap } from './show-datetime';
 
 describe('getPageTitleMap', () => {
-  it('should work', () => {
-    expect(getPageTitleMap({ Project: { pages: [{ title: 'hello' }, {}, { title: 'world' }, {}] } } as any)).toEqual(
+  test('should work', () => {
+    expect(getPageTitleMap([{ title: 'hello' }, {}, { title: 'world' }, {}] as any)).toEqual(
       new Map([
         ['hello', { title: 'hello' }],
         ['world', { title: 'world' }],
