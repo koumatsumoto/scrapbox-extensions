f19af8edd5b3ea638700e0db01a250475021613a
Kou Matsumoto <developer@kou-matsumoto.jp>
2021-09-16 09:29:37 +0900

    feat(scripts): support list item image from config

--- src/scripts/custom-list-style/set-default-image.ts
+++ src/scripts/custom-list-style/set-default-image.ts
@@ -1,12 +1,16 @@
-import { timer } from 'rxjs';
+import { timer, withLatestFrom } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { DynamicConfig } from 'scrapbox-tools';
 import { defaultImageUrl } from './constants';
 
-const appendIcon = (elem: HTMLElement) => {
-  const container = document.createElement('div');
-  container.classList.add('icon');
-  container.innerHTML = `<img loading="lazy" src="${defaultImageUrl}">`;
-  elem.appendChild(container);
-};
+const appendIcon =
+  (imageUrl: unknown = defaultImageUrl) =>
+  (elem: HTMLElement) => {
+    const container = document.createElement('div');
+    container.classList.add('icon');
+    container.innerHTML = `<img loading="lazy" src="${imageUrl}">`;
+    elem.appendChild(container);
+  };
 
 const hasNoIcon = (elem: HTMLElement) => {
   return elem.querySelector('.icon') === null;
@@ -15,13 +19,15 @@ const hasNoIcon = (elem: HTMLElement) => {
 /**
  * Add <img> into list item that does not have own image.
  */
-export const setDefaultImage = () => {
-  timer(0, 1000 * 5).subscribe(() => {
-    if (window.scrapbox.Layout !== 'list') {
-      return;
-    }
+export const setDefaultImage = ({ dynamicConfig }: { dynamicConfig: DynamicConfig }) => {
+  timer(0, 1000 * 5)
+    .pipe(
+      withLatestFrom(dynamicConfig.data),
+      filter(() => window.scrapbox.Layout !== 'list'),
+    )
+    .subscribe(([, config]) => {
+      const listElements = Array.from(document.querySelectorAll<HTMLElement>('.page-list .page-list-item .content'));
 
-    const listElements = Array.from(document.querySelectorAll<HTMLElement>('.page-list .page-list-item .content'));
-    listElements.filter(hasNoIcon).forEach(appendIcon);
-  });
+      listElements.filter(hasNoIcon).forEach(appendIcon(config['defaultListItemImage']));
+    });
 };
